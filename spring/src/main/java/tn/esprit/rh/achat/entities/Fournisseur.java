9aeb395bd97c256e18fe292d70a2df6bc7686569
yessine09 <yessine.akaichi@esprit.tn>
2022-11-11 14:13:53 +0100

    Revert "thirrrrrrd committ"
    This reverts commit 7a595d6c5d3c2a302cf6198ca84e79aab4e53bc7.

--- spring/src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
+++ spring/src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
@@ -15,7 +15,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,7 +24,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Fournisseur implements Serializable {
 
 	private static final long serialVersionUID = 1L;
