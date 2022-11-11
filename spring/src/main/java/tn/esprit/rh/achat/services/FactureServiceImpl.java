e85cffd7ce4834dc9fe1f23853d2d2041af7eb1d
zaineb <zaineb.boussaid@esprit.tn>
2022-11-11 03:15:37 +0100

    docker

--- spring/src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java
+++ spring/src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java
@@ -52,7 +52,7 @@ public class FactureServiceImpl implements IFactureService {
 		float montantRemise = 0;
 		for (DetailFacture detail : detailsFacture) {
 			//Récuperer le produit 
-			Produit produit = produitRepository.findById(detail.getProduit().getIdProduit()).get();
+			Produit produit = produitRepository.findById(detail.getProduit().getIdProduit()).orElse(null);
 			//Calculer le montant total pour chaque détail Facture
 			float prixTotalDetail = detail.getQteCommandee() * produit.getPrix();
 			//Calculer le montant remise pour chaque détail Facture
@@ -93,7 +93,9 @@ public class FactureServiceImpl implements IFactureService {
 	@Override
 	public List<Facture> getFacturesByFournisseur(Long idFournisseur) {
 		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(null);
-		return (List<Facture>) fournisseur.getFactures();
+
+		   assert (fournisseur!=null);
+			return (List<Facture>) fournisseur.getFactures();
 	}
 
 	@Override
